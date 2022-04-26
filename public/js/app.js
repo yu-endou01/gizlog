da9535b996fc0dc8cddae60c96d60b35143fd8cd
goshima <>
2022-04-26 19:55:51 +0900

    remove: csrfを埋め込む処理はmasterに書かないほうが良い(研修生自信に書かせたい)

--- public/js/app.js
+++ public/js/app.js
@@ -1033,12 +1033,6 @@ window.axios.defaults.headers.common = {
   'X-Requested-With': 'XMLHttpRequest'
 };
 
-$.ajaxSetup({
-  headers: {
-    "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("value"),
-  }
-});
-
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
