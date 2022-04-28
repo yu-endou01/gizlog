c8c8dda0be58bd51f43868a44d5ae6a6269a4e9b
goshima <>
2022-04-28 11:04:58 +0900

    refacotr: 削除処理修正。context:thisは使わない

--- public/js/custom.js
+++ public/js/custom.js
@@ -135,16 +135,16 @@ $(function(){
 
   // 質問掲示板マイページ削除機能
   $('.question-delete-button').on('click', function(e) {
+    const selectedSelector = $(this);
     e.preventDefault();
     const url = $(this).parent().attr('action');
     const methodType = $(this).prev('input[type="method"]').attr('method');
     $.ajax({
       url: url,
       type: methodType,
-      context : this, // done以降で$(this)を使うため
     })
     .done(function () {
-      $(this).parents('.row').remove(); // 該当レコードを削除
+      selectedSelector.parents('.row').remove(); // 該当レコードを削除
     })
   });
 
