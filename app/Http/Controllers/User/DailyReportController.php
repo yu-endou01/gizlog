d63d9211cb2c36685ab0f7d89305dcc7233c19de
阿部大祐 <abe@gizumo-inc.jp>
2022-06-23 19:37:51 +0900

    add: Controllerでコンストラクタインジェクション

--- app/Http/Controllers/User/DailyReportController.php
+++ app/Http/Controllers/User/DailyReportController.php
@@ -4,8 +4,14 @@ namespace App\Http\Controllers\User;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\DailyReport;
 
 class DailyReportController extends Controller
 {
-    //
+    private $dailyReport;
+
+    public function __construct(DailyReport $dailyReport)
+    {
+        $this->dailyReport = $dailyReport;
+    }
 }
