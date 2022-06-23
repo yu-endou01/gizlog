c637fa231e516a50551c307dabdea9c0e4e544d2
阿部大祐 <abe@gizumo-inc.jp>
2022-06-23 19:14:43 +0900

    fix: 全テーブルのプライマリーキーをbigIncrementsに変更

--- database/migrations/2017_03_14_084054_create_admin_users_table.php
+++ database/migrations/2017_03_14_084054_create_admin_users_table.php
@@ -14,7 +14,7 @@ class CreateAdminUsersTable extends Migration
     public function up()
     {
         Schema::create('admin_users', function (Blueprint $table) {
-            $table->increments('id');
+            $table->bigIncrements('id');
             $table->string('name');
             $table->string('password');
             $table->integer('user_info_id');
