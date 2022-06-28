9bdaf6a0bd09de57f33737dde4d690ab17e88c47
阿部大祐 <abe@gizumo-inc.jp>
2022-06-28 18:38:34 +0900

    fix: 日報詳細画面のつなぎ込み作成

--- resources/views/user/daily_report/show.blade.php
+++ resources/views/user/daily_report/show.blade.php
@@ -5,18 +5,18 @@
 <div class="main-wrap">
   <div class="panel panel-success">
     <div class="panel-heading">
-      金谷の日報
+      {{ $dailyReport->reporting_time->format('Y/m/d（D）') }}の日報
     </div>
     <div class="table-responsive">
       <table class="table table-striped table-bordered">
         <tbody>
           <tr>
             <th class="table-column">Title</th>
-            <td class="td-text"></td>
+            <td class="td-text">{{ $dailyReport->title }}</td>
           </tr>
           <tr>
             <th class="table-column">Content</th>
-            <td class='td-text'></td>
+            <td class='td-text'>{!! nl2br(e($dailyReport->content)) !!}</td>
           </tr>
         </tbody>
       </table>
