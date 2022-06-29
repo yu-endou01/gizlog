75f09274df44ea1a89c42bb1d7f10ac7abdcd143
阿部大祐 <abe@gizumo-inc.jp>
2022-06-29 21:09:02 +0900

    fix: reporting_time初期値を本日に変更

--- resources/views/user/daily_report/create.blade.php
+++ resources/views/user/daily_report/create.blade.php
@@ -6,7 +6,7 @@
   <div class="container">
     {{ Form::open(['route' => 'report.store', 'method' => 'POST']) }}
       <div class="form-group form-size-small has-error">
-        {{ Form::date('reporting_time', null, ['class' => 'form-control']) }}
+        {{ Form::date('reporting_time', date('Y-m-d'), ['class' => 'form-control']) }}
         <span class="help-block"></span>
       </div>
       <div class="form-group has-error">
